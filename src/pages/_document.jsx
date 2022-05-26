ec085efc66f6d1a0fe3b61abfd7610e03ece8c0b
umgustavo <gugu.r.azevedo@gmail.com>
2022-05-26 14:46:45 -0300

    Add emoji favicon

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -40,6 +40,11 @@ export default class MyDocument extends Document {
                         href='https://fonts.googleapis.com/css2?family=Catamaran:wght@300;400;500;700&display=swap'
                         rel='stylesheet'
                     />
+
+                    <link
+                        rel='icon'
+                        href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⏰</text></svg>'
+                    />
                 </Head>
                 <body>
                     <Main />
